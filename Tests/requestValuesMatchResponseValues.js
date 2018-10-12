773f20ee137c6951bcb04be924e4078a1ce3d7ff
Thomas Chaplin <35810778+thomaschaplin@users.noreply.github.com>
2018-10-12 09:26:29 +0100

    Update requestValuesMatchResponseValues.js

--- Tests/requestValuesMatchResponseValues.js
+++ Tests/requestValuesMatchResponseValues.js
@@ -3,10 +3,10 @@ var request_keys = _.keys(request_body_first_object)
 var response_body = (JSON.parse(responseBody))
 var response_body_first_object = _.first(response_body.data)
 
-f = function(x, y, key_name) {
-    return pm.test(`Request value ${key_name} matches response value`, pm.expect(x).to.eql(y))
+requestValuesMatchResponseValues = function(request_value, response_value, key_name) {
+    return pm.test(`Request value ${key_name} matches response value`, pm.expect(request_value).to.eql(response_value))
 }
 
 _.map(request_keys, (key) => {
-    f(request_body_first_object[key], response_body_first_object[key], key)
+    requestValuesMatchResponseValues(request_body_first_object[key], response_body_first_object[key], key)
 })
